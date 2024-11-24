924e9de74a2ab0147eaa14b9becdf0a106ac4d12
Stranger1298 <amanraj89969@gmail.com>
2024-11-24 19:21:11 +0530

    added backend

---
+++ server/models/Todo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    todo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    priority: {
+        type: String,
+        enum: ['low', 'normal', 'high'],
+        default: 'normal'
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
